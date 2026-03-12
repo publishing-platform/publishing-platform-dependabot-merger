723adca642a071f4bda12aac1398e5f5c5e36d8b
neilha <neilharrold@gmail.com>
2026-03-12 09:29:55 +0000

    Implement AutoMerger module with methods for merging Dependabot PRs and analyzing PRs

---
+++ lib/auto_merger.rb
@@ -0,0 +1,61 @@
+require_relative "./policy_manager"
+require_relative "./repo"
+
+module AutoMerger
+  def self.invoke_merge_script!
+    merge_dependabot_prs(dry_run: false)
+  end
+
+  def self.pretend_invoke_merge_script!
+    puts "🍸 Doing a dry run of the auto merge script 🏃"
+    merge_dependabot_prs(dry_run: true)
+  end
+
+  def self.merge_dependabot_prs(dry_run: false)
+    Repo.all.each do |repo|
+      policy_manager = PolicyManager.new(repo.publishing_platform_dependabot_merger_config)
+
+      if !policy_manager.remote_config_exists?
+        puts "#{repo.name}: the remote .publishing_platform_dependabot_merger.yml file is missing."
+      elsif !policy_manager.valid_remote_config_syntax?
+        puts "#{repo.name}: the remote .publishing_platform_dependabot_merger.yml YAML syntax is corrupt."
+      elsif !policy_manager.remote_config_api_version_supported?
+        puts "#{repo.name}: the remote .publishing_platform_dependabot_merger.yml file is using an unsupported API version."
+      elsif repo.dependabot_pull_requests.count.zero?
+        puts "#{repo.name}: no Dependabot PRs found."
+      else
+        puts "#{repo.dependabot_pull_requests.count} Dependabot PRs found for repo '#{repo.name}':"
+
+        repo.dependabot_pull_requests.each do |pr|
+          puts "  - Inspecting #{repo.name}##{pr.number}..."
+
+          merge_dependabot_pr(pr, policy_manager, dry_run:)
+        end
+      end
+    end
+  end
+
+  def self.merge_dependabot_pr(pull_request, policy_manager, dry_run: true)
+    if !pull_request.is_auto_mergeable?
+      puts "    ...bad PR: #{pull_request.reasons_not_to_merge.join(' ')} Skipping."
+    elsif !policy_manager.is_auto_mergeable?(pull_request)
+      puts "    ...auto-merging is against policy: #{policy_manager.reasons_not_to_merge(pull_request).join(' ')} Skipping."
+    elsif dry_run
+      puts "    ...eligible for auto-merge! This is a dry run, so skipping."
+    else
+      puts "    ...approving! ✅"
+      pull_request.approve!
+      puts "    ...merging! 🎉"
+      pull_request.merge!
+    end
+  end
+
+  def self.analyse_dependabot_pr(url)
+    puts "Analysing #{url}..."
+    _, repo_name, pr_number = url.match(/publishing-platform\/(.+)\/pull\/(.+)$/).to_a
+    repo = Repo.new(repo_name)
+    pr = repo.dependabot_pull_request(pr_number)
+    policy_manager = PolicyManager.new(repo.publishing_platform_dependabot_merger_config)
+    merge_dependabot_pr(pr, policy_manager, dry_run: true)
+  end
+end
---
+++ spec/lib/auto_merger_spec.rb
@@ -0,0 +1,166 @@
+require "date"
+require_relative "../../lib/auto_merger"
+
+RSpec.describe AutoMerger do
+  before :each do
+    allow($stdout).to receive(:puts) # suppress console output as it gets noisy
+  end
+
+  let(:policy_manager) do
+    mock_policy_manager = instance_double("PolicyManager")
+    allow(mock_policy_manager).to receive(:remote_config_exists?).and_return(true)
+    allow(mock_policy_manager).to receive(:valid_remote_config_syntax?).and_return(true)
+    allow(mock_policy_manager).to receive(:remote_config_api_version_supported?).and_return(true)
+    allow(mock_policy_manager).to receive(:is_auto_mergeable?).and_return(true)
+    allow(PolicyManager).to receive(:new).and_return(mock_policy_manager)
+    mock_policy_manager
+  end
+
+  describe ".invoke_merge_script!" do
+    it "should call `merge_dependabot_prs` with `dry_run: false`" do
+      expect(AutoMerger).to receive(:merge_dependabot_prs).with(dry_run: false)
+      AutoMerger.invoke_merge_script!
+    end
+  end
+
+  describe ".pretend_invoke_merge_script!" do
+    it "should call `merge_dependabot_prs` with `dry_run: true`" do
+      expect(AutoMerger).to receive(:merge_dependabot_prs).with(dry_run: true)
+      AutoMerger.pretend_invoke_merge_script!
+    end
+  end
+
+  describe ".merge_dependabot_prs" do
+    let(:mock_pr) do
+      instance_double("PullRequest", number: 1, is_auto_mergeable?: true, approve!: true, merge!: true)
+    end
+
+    it "iterates through all Dependabot PRs of all Repos" do
+      mock_pr_1 = instance_double("PullRequest", number: 1)
+      mock_pr_2 = instance_double("PullRequest", number: 2)
+      mock_pr_3 = instance_double("PullRequest", number: 3)
+      mock_repo_1 = instance_double("Repo", name: "Repo 1", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr_1])
+      mock_repo_2 = instance_double("Repo", name: "Repo 2", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr_2, mock_pr_3])
+      allow(Repo).to receive(:all).and_return([
+        mock_repo_1,
+        mock_repo_2,
+      ])
+
+      expect(AutoMerger).to receive(:merge_dependabot_pr).with(mock_pr_1, policy_manager, dry_run: false)
+      expect(AutoMerger).to receive(:merge_dependabot_pr).with(mock_pr_2, policy_manager, dry_run: false)
+      expect(AutoMerger).to receive(:merge_dependabot_pr).with(mock_pr_3, policy_manager, dry_run: false)
+
+      AutoMerger.merge_dependabot_prs
+    end
+
+    it "forwards the `dry_run` keyword arg if passed" do
+      mock_pr = instance_double("PullRequest", number: 1)
+      mock_repo = instance_double("Repo", name: "Repo", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr])
+      allow(Repo).to receive(:all).and_return([mock_repo])
+
+      expect(AutoMerger).to receive(:merge_dependabot_pr).with(mock_pr, policy_manager, dry_run: true)
+
+      AutoMerger.merge_dependabot_prs(dry_run: true)
+    end
+
+    it "should make a call to PolicyManager.remote_config_exists?, which should block merge if false" do
+      mock_repo = instance_double("Repo", name: "Repo 1", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr])
+      allow(Repo).to receive(:all).and_return([mock_repo])
+
+      allow(policy_manager).to receive(:remote_config_exists?).and_return(false)
+
+      expect(policy_manager).to receive(:remote_config_exists?)
+      expect(AutoMerger).to_not receive(:merge_dependabot_pr)
+      expect { AutoMerger.merge_dependabot_prs }.to output("Repo 1: the remote .publishing_platform_dependabot_merger.yml file is missing.\n").to_stdout
+    end
+
+    it "should make a call to PolicyManager.valid_remote_config_syntax?, which should block merge if false" do
+      mock_repo = instance_double("Repo", name: "Repo 1", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr])
+      allow(Repo).to receive(:all).and_return([mock_repo])
+
+      allow(policy_manager).to receive(:valid_remote_config_syntax?).and_return(false)
+
+      expect(policy_manager).to receive(:valid_remote_config_syntax?)
+      expect(AutoMerger).to_not receive(:merge_dependabot_pr)
+      expect { AutoMerger.merge_dependabot_prs }.to output("Repo 1: the remote .publishing_platform_dependabot_merger.yml YAML syntax is corrupt.\n").to_stdout
+    end
+
+    it "should make a call to PolicyManager.remote_config_api_version_supported?, which should block merge if false" do
+      mock_repo = instance_double("Repo", name: "Repo 1", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [mock_pr])
+      allow(Repo).to receive(:all).and_return([mock_repo])
+
+      allow(policy_manager).to receive(:remote_config_api_version_supported?).and_return(false)
+
+      expect(policy_manager).to receive(:remote_config_api_version_supported?)
+      expect(AutoMerger).to_not receive(:merge_dependabot_pr)
+      expect { AutoMerger.merge_dependabot_prs }.to output("Repo 1: the remote .publishing_platform_dependabot_merger.yml file is using an unsupported API version.\n").to_stdout
+    end
+
+    it "should announce when no Dependabot PRs were found" do
+      policy_manager # set up the mock policy manager object
+      mock_repo = instance_double("Repo", name: "Repo 1", publishing_platform_dependabot_merger_config: {}, dependabot_pull_requests: [])
+      allow(Repo).to receive(:all).and_return([mock_repo])
+
+      expect(AutoMerger).to_not receive(:merge_dependabot_pr)
+      expect { AutoMerger.merge_dependabot_prs }.to output("Repo 1: no Dependabot PRs found.\n").to_stdout
+    end
+  end
+
+  describe ".merge_dependabot_pr" do
+    let(:mock_pr) do
+      mock_pr = instance_double("PullRequest")
+      allow(mock_pr).to receive(:is_auto_mergeable?).and_return(true)
+      mock_pr
+    end
+
+    def expect_no_merge(mock_pr)
+      expect(mock_pr).to_not receive(:approve!)
+      expect(mock_pr).to_not receive(:merge!)
+    end
+
+    it "should make a call to PolicyManager.is_auto_mergeable?, which should block merge if false" do
+      allow(policy_manager).to receive(:is_auto_mergeable?).and_return(false)
+      allow(policy_manager).to receive(:reasons_not_to_merge).and_return(["Foo."])
+
+      expect_no_merge(mock_pr)
+      expect { AutoMerger.merge_dependabot_pr(mock_pr, policy_manager, dry_run: false) }.to output(
+        "    ...auto-merging is against policy: Foo. Skipping.\n",
+      ).to_stdout
+    end
+
+    it "should make a call to PullRequest's `is_auto_mergeable?`, which should block merge if false" do
+      allow(mock_pr).to receive(:is_auto_mergeable?).and_return(false)
+      allow(mock_pr).to receive(:reasons_not_to_merge).and_return(["Bar."])
+
+      expect_no_merge(mock_pr)
+      expect { AutoMerger.merge_dependabot_pr(mock_pr, policy_manager, dry_run: false) }.to output(
+        "    ...bad PR: Bar. Skipping.\n",
+      ).to_stdout
+    end
+
+    it "avoids approving or merging when in dry run mode" do
+      expect_no_merge(mock_pr)
+      AutoMerger.merge_dependabot_pr(mock_pr, policy_manager, dry_run: true)
+    end
+
+    it "calls approve! and merge! when not in dry run mode" do
+      expect(mock_pr).to receive(:approve!)
+      expect(mock_pr).to receive(:merge!)
+      AutoMerger.merge_dependabot_pr(mock_pr, policy_manager, dry_run: false)
+    end
+  end
+
+  describe ".analyse_dependabot_pr" do
+    it "uses `merge_dependabot_pr` under the hood" do
+      mock_pr = instance_double("PullRequest")
+      mock_repo = instance_double("Repo", dependabot_pull_request: mock_pr, publishing_platform_dependabot_merger_config: {})
+      mock_policy_manager = {}
+      allow(Repo).to receive(:new).with("foo").and_return(mock_repo)
+      allow(PolicyManager).to receive(:new).and_return(mock_policy_manager)
+
+      expect(mock_repo).to receive(:dependabot_pull_request).with("1234")
+      expect(AutoMerger).to receive(:merge_dependabot_pr).with(mock_pr, mock_policy_manager, dry_run: true)
+      AutoMerger.analyse_dependabot_pr("https://github.com/publishing-platform/foo/pull/1234")
+    end
+  end
+end
